d935ce5343d03128ca1c05e788ef83af0c151b28
Yang_Lei_ <452857163@qq.com>
2024-04-12 21:19:33 +0800

    2.3和2.4完成

--- 第2章 Rust导览/2.2 Rust函数/code/gcd/target/debug/gcd.d
+++
@@ -1,1 +0,0 @@
-/Users/yanglei/Desktop/ProgrammingRUST/第2章\ Rust导览/2.2\ Rust函数/code/gcd/target/debug/gcd: /Users/yanglei/Desktop/ProgrammingRUST/第2章\ Rust导览/2.2\ Rust函数/code/gcd/src/main.rs
---
+++ 第2章 Rust导览/2.3 编写与运行单元测试/code/test_gcd/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let g = gcd(15, 5);
+    println!("gcd(15, 5) = {}", g);
+}
+
+// 本函数用于计算两个数的最大公约数
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+// 本函数用于测试最大公约数
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11);
+}
